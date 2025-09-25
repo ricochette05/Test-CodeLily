5f2304036bc8cc8a22482b3d775c23201a764372
ricochette05 <palaparjoy@gmail.com>
2025-09-26 00:45:56 +0800

    Added baseURL

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,31 +2,38 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
-  timeout: 30 * 1000, // max time each test can run
+  timeout: 30 * 1000,
   expect: {
-    timeout: 5000, // max time expect() waits
+    timeout: 5000,
   },
+  // Set the base URL for tests — so you can use relative paths in page.goto
   use: {
     headless: true,
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
     trace: 'retain-on-failure',
+    baseURL: 'https://grand-youtiao-2114df.netlify.app',
   },
   reporter: [['list'], ['html']],
 
-  // ✅ Run tests across multiple browsers
   projects: [
     {
       name: 'Chromium',
-      use: { ...devices['Desktop Chrome'] },
-    },
+      use: {
+        ...devices['Desktop Chrome'],
+      },
+    // },
     // {
     //   name: 'Firefox',
-    //   use: { ...devices['Desktop Firefox'] },
+    //   use: {
+    //     ...devices['Desktop Firefox'],
+    //   },
     // },
     // {
     //   name: 'WebKit',
-    //   use: { ...devices['Desktop Safari'] },
+    //   use: {
+    //     ...devices['Desktop Safari'],
+    //   },
     // },
   ],
 });
